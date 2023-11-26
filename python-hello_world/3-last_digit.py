a7062c02006bfe6e4dec37163c1cb66638839cb6
HibatAllah5 <hibakhaldoune5@gmail.com>
2023-11-26 21:06:16 +0100

    Last digit changes

--- python-hello_world/3-last_digit.py
+++ python-hello_world/3-last_digit.py
@@ -1,7 +1,7 @@
 #This program will assign a random signed number to the variable number each time it is executed.
 import random
 number = random.randint(-10000, 10000)
-Last_digit = (repr(number)[-1])
+Last_digit = abs(number) % 10
 if number < 0:
     negative_number =  Last_digit * -1
     print("Last digit of", number, "is", negative_number , "and is less than 6 and not 0")

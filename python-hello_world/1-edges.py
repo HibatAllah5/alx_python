8fc19c7852002d3ffabf63f90e2fe65bcfa51499
HibatAllah5 <hibakhaldoune5@gmail.com>
2023-11-26 16:44:03 +0100

    Holberton

--- python-hello_world/1-edges.py
+++ python-hello_world/1-edges.py
@@ -1,7 +1,7 @@
 word = "Holberton"
-word_first_3 = " First 3 letters : Hol"
-word_last_2 = "Last 2 letters : on"
-middle_word = "Middle word : olberto"
+word_first_3 = "First 3 letters: Hol"
+word_last_2 = "Last 2 letters: on"
+middle_word = "Middle word: olberto"
 
 print(word_first_3)
 print(word_last_2)

659b8d646bd67d01918e13568d278d045d0d70f8
HibatAllah5 <hibakhaldoune5@gmail.com>
2023-12-02 21:41:45 +0100

    import modules

---
+++ python-import_modules/0-add.py
@@ -0,0 +1,9 @@
+if __name__ == "__main__":
+    a = 1
+    b = 2
+
+from add_0 import add
+
+c = add(a, b)
+
+print("{:d} + {:d} = {:d}".format(a, b, c))
---
+++ python-import_modules/add_0.py
@@ -0,0 +1,3 @@
+# Import a simple function from a simple file.
+def add(a, b):
+    return a + b

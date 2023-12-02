78e06633e733e9c4b064621461a7c1c0a5d71fe9
HibatAllah5 <hibakhaldoune5@gmail.com>
2023-12-02 23:37:30 +0100

    changes

--- python-import_modules/5-raise_exception_msg.py
+++ python-import_modules/5-raise_exception_msg.py
@@ -1,7 +1,3 @@
 #raise message
 def raise_exception_msg(message=""):
-    raise NameError("C is fun")
-def raise_exception_msg(message=""):
-    raise NameError("Python is cool")
-def raise_exception_msg(message=""):
-    raise NameError(" ")
+    raise TypeError("message")

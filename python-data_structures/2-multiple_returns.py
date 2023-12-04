fffda0e402e2a7bfc7b6a68eadc5b2c6222ecb6c
HibatAllah5 <hibakhaldoune5@gmail.com>
2023-12-05 00:41:18 +0100

    new

--- python-data_structures/2-multiple_returns.py
+++ python-data_structures/2-multiple_returns.py
@@ -1,6 +1,8 @@
-
+#Function that returns a tuple with the length of a string and its first character.
 def multiple_returns(sentence):
-    for sentence:(NameError):
-        print(sentence: None)
+    size = len(sentence)
+    if not size:
+        return ( size, None)
+    else:
+        return ( size, sentence[0])
     
-

05c70bb6f465a9547854ffe198fb87de015e9413
HibatAllah5 <hibakhaldoune5@gmail.com>
2023-12-05 00:27:46 +0100

    new

--- python-data_structures/1-print_matrix_integer.py
+++ python-data_structures/1-print_matrix_integer.py
@@ -1,15 +1,10 @@
 #Function that prints a matrix of integers.  
 def print_matrix_integer(matrix=[[]]):
-    Row = int(input("Enter the number of rows:"))
-    Column = int(input("Enter the number of columns:"))
-    matrix = [[]]
-    for row in range(Row):
-        a = []
-        for column in range(Column):
-            a.append(int(input()))
-        matrix.append(a)    
-
-    for row in range(Row):
-        for column in range(Column):
-            print(matrix[row][column], end=" ")
-        print([".format(", "{:d}"])    
+    if matrix == [[]]:
+        print()
+    else:
+        for row in matrix:
+            for column in row:
+                print('{:d}'.format(column), end=' '
+                       if column != row[-1] else '\n') 
+                

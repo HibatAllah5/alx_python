4ee5f5837545d63b98a84a1c4b2d919a26a1105d
HibatAllah5 <hibakhaldoune5@gmail.com>
2023-12-04 22:41:23 +0100

    n

--- python-data_structures/0-no_c.py
+++ python-data_structures/0-no_c.py
@@ -2,5 +2,5 @@
 def no_c(my_string):
     my_string = ' '
     for letters in my_string:
-        print(my_string.translate({no_c(letters): None for letters in 'Cc'}))
+        print(my_string.translate({no_c(letters): None for letters in 'C, c'}))
     return my_string

3afba406f45a6128300b1dfe66be366e29c112ad
HibatAllah5 <hibakhaldoune5@gmail.com>
2023-12-11 22:05:01 +0100

    new

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,7 +1,3 @@
-#Class Square that defines a square with his size
 class Square:
-    def __init__(self, size):
-        self.size = size
-        Square1 = Square("size")
-print(Square.size)  
-
+    """Simple empty square class"""
+    pass

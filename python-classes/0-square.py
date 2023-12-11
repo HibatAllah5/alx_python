188709535b8b633312a11cea1f0f9573afa581c2
HibatAllah5 <hibakhaldoune5@gmail.com>
2023-12-11 22:40:14 +0100

    new

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,12 +1,7 @@
 class Square:
-    """Simple square class with his size as a field"""
-
     def __init__(self, size):
-        """ Instance the class Square
-            Arguments:
-                @size: the size of every side of the Square"""
         self.size = size
 
-mysquare = Square(" ")
+mysquare = Square("size: ")
 
 print(mysquare.size)

67c9b124de902762361a0e7af33dfedfd388ea91
HibatAllah5 <hibakhaldoune5@gmail.com>
2023-12-11 23:54:52 +0100

    changes

--- python-inheritance/3-base_geometry.py
+++ python-inheritance/3-base_geometry.py
@@ -1,7 +1,17 @@
-"""Simple module with the definition of an empty class.
+"""Simple module with the definition of an empty class with an unimplemented
+method.
 """
 
 
 class BaseGeometry():
     """Simple empty BaseGeometry class"""
-    pass
+
+    def area(self):
+        """Prototype of the area function that actually isn't implemented.
+
+        Raises:
+            Exception:  a simple exception that tells that the area function is
+                        not implmenet yet.
+        """
+        raise Exception('area() is not implemented')
+    

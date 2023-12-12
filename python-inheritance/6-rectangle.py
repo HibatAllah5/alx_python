0e27138910c13078b8467f932bb810f4f6185459
HibatAllah5 <hibakhaldoune5@gmail.com>
2023-12-13 00:31:57 +0100

    hello new

--- python-inheritance/6-rectangle.py
+++ python-inheritance/6-rectangle.py
@@ -1,24 +1,34 @@
-"""Simple module with a Rectangle class that inherits BaseGeometry.
+"""Simple module with the definition of an empty class with an unimplemented
+method and a validator function.
 """
-BaseGeometry = __import__('5-base_geometry').BaseGeometry
 
-"""Simple class Rectangle with width, height and input validation.
-"""
-class Rectangle(BaseGeometry):
-         """Initialize the rectangle after checking if the values passed are
-         valid integers using it's superclass method.
 
-         Arguments:
-             width   {int}   --  a positive non zero integer.
-             height  {int}   --  a positive non zero integer.
+class BaseGeometry():
+    """Simple empty BaseGeometry class"""
 
-         Raises:
-             TypeError:  if any value is not an integer.
-             ValueError: if any value is an integer less or equal to zero.
-         """
-         def __init__(self, width, height):
-             super().__init__()
-             super().integer_validator('width', width)
-             super().integer_validator('height', height)
-             self.__width = width
-             self.__height = height
+    def area(self):
+        """Prototype of the area function that actually isn't implemented.
+
+        Raises:
+            Exception:  a simple exception that tells that the area function is
+                        not implmenet yet.
+        """
+        raise Exception('area() is not implemented')
+
+    def integer_validator(self, name, value):
+        """Verify if value is a valid integer value.
+
+        Arguments:
+            name {str}      --  a string related with the value.
+            value {object}  --  the value that will be checked.
+
+        Raises:
+            TypeError:  if value is not an integer.
+            ValueError: if value is an integer less or equal to zero.
+        """
+        if name is not None and len(name) > 0:
+            if type(value) is not int:
+                raise TypeError('{:s} must be an integer'.format(name))
+            if value <= 0:
+                raise ValueError('{:s} must be greater than 0'.format(name))
+            
--- python-inheritance/7-rectangle.py
+++ python-inheritance/7-rectangle.py
@@ -1,13 +1,13 @@
 """Simple module with a Rectangle class that inherits BaseGeometry.
 """
-BaseGeometry = __import__('5-base_geometry').BaseGeometry
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 
-
-"""Simple class Rectangle with width, height and input validation.
-"""
 class Rectangle(BaseGeometry):
-        
+    """Simple class Rectangle with width, height and input validation.
+    """
+
+    def __init__(self, width, height):
         """Initialize the rectangle after checking if the values passed are
         valid integers using it's superclass method.
 
@@ -19,33 +19,9 @@ class Rectangle(BaseGeometry):
             TypeError:  if any value is not an integer.
             ValueError: if any value is an integer less or equal to zero.
         """
-
-        def __init__(self, width, height):
-
-
-            super().__init__()
-            super().integer_validator('width', width)
-            super().integer_validator('height', height)
-            self.__width = width
-            self.__height = height
-
+        super().__init__()
+        super().integer_validator('width', width)
+        super().integer_validator('height', height)
+        self.__width = width
+        self.__height = height
         
-        """Computes the Area of a Rectangle.
-        Based on the formula AreaRectangle = Height * Width
-
-        Returns:
-            {int} -- The integer value of the Rectangle's Area.
-        """
-        def area(self):
-            return self.__height * self.__width
-
-       
-        """Generates a human readable string that represents the Rectangle
-
-        Returns:
-            {str} -- The string representation of the Rectangel using the #
-                        character, it represents a 2d rectangle.
-        """
-        def __str__(self):
-            return '[Rectangle] {:d}/{:d}'.format(self.__width, self.__height)
-    

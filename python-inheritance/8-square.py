1c6fee78c7f5e201210fccb76b3bd4dfdd891629
HibatAllah5 <hibakhaldoune5@gmail.com>
2023-12-13 00:46:29 +0100

    NEW

--- python-inheritance/8-square.py
+++ python-inheritance/8-square.py
@@ -1,8 +1,15 @@
+"""Defines a Rectangle subclass Square."""
 Rectangle = __import__('7-rectangle').Rectangle
+
 
 """Represent a square."""
 class Square(Rectangle):
 
+    """Initialize a new square.
+
+        Args:
+            size (int): The size of the new square.
+    """
     def __init__(self, size):
         
         self.integer_validator("size", size)

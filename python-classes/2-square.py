c1fc3229ac6768ec0b35083ea3fe9ad91febbd41
HibatAllah5 <hibakhaldoune5@gmail.com>
2023-12-12 23:26:57 +0100

    changes

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -1,17 +1,17 @@
-class Square:
-    """Defines a square"""
+"""Defines a square"""
+class Square():
 
+    """Initialises the data"""
     def __init__(self, size=0):
-        """Initialises the data"""
-
         
         if type(size) != int:
             raise TypeError("size must be an integer")
         if size < 0:
             raise ValueError("size must be >= 0")
         self.__size = size
-
+        
+    """Returns current square area"""
     def area(self):
-        """Returns current square area"""
+        
         return (self.__size * self.__size)
     

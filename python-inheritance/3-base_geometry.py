f2bf2d189e4a53f2cfd072c077b591807c9bbda0
HibatAllah5 <hibakhaldoune5@gmail.com>
2023-12-13 00:24:32 +0100

    new

--- python-inheritance/3-base_geometry.py
+++ python-inheritance/3-base_geometry.py
@@ -1,10 +1,4 @@
-"""Simple module with the definition of an empty class.
-"""
-
-BaseGeometry = __import__('3-base_geometry').BaseGeometry
-
-"""Simple empty BaseGeometry class"""
 class BaseGeometry:
-    
+    """Empty class
+    """
     pass
-

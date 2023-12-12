1e9b16f65e0dd3ece337c26bbb0e6b39e63bc959
HibatAllah5 <hibakhaldoune5@gmail.com>
2023-12-12 22:32:41 +0100

    new

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,4 +1,4 @@
-class Square:
+class _square:
     """Defines a square"""
 
     def __init__(self, size):
@@ -9,3 +9,6 @@ class Square:
         """
 
         self.__size = size
+
+        return type(self) is size
+    

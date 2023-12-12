5b97b4a912e35148908344ba3b0ef83737bb6197
HibatAllah5 <hibakhaldoune5@gmail.com>
2023-12-12 23:20:47 +0100

    new

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,14 +1,9 @@
-class _square:
-    """Defines a square"""
+"""Defines a square"""
+
+class Square:
+    """Define a private attribute size"""
 
     def __init__(self, size):
-        """Initialises the data.
-
-        Args:
-            size (int): The size of the new square.
-        """
-
         self.__size = size
-
-        return type(self) is size
+        
     

80918bf42a60a9985f11ffc1308378c9a1520817
HibatAllah5 <hibakhaldoune5@gmail.com>
2023-12-13 23:04:29 +0100

    7

--- python-inheritance/7-rectangle.py
+++ python-inheritance/7-rectangle.py
@@ -1,28 +1,31 @@
-"""Simple module with a Rectangle class that inherits BaseGeometry.
 """
+    Rectangle module
+"""
+BaseGeometry = __import__('5-base_geometry').BaseGeometry
 
-class Rectangle():
-    """Simple class Rectangle with width, height and input validation.
+
+class Rectangle(BaseGeometry):
     """
-
+    class  that inherits from BaseGeometry
+    """
     def __init__(self, width, height):
-        """Initialize the rectangle 
         """
-        super().__init__()
-        super().integer_validator('width', width)
-        super().integer_validator('height', height)
+            Initialize rectangle from BaseGeometry
+        """
+        self.integer_validator('width', width)
+        self.integer_validator('height', height)
         self.__width = width
         self.__height = height
 
-    
-        """Computes the Area of a Rectangle.
-        """
     def area(self):
-        return self.__height * self.__width
+        """
+        define the area of The rectangle
+        """
+        return self.__width * self.__height
 
-    
-    """Generates a human readable string that represents the Rectangle
-    """
     def __str__(self):
-        return '[Rectangle] {:d}/{:d}'.format(self.__width, self.__height)
+        """
+            Rectangle Descreption
+        """
+        return("[Rectangle] {}/{}".format(self.__width, self.__height))
     

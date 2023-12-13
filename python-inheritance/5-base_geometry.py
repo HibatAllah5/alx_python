95f7672e56201177234315d5eb2a5c45a14b75b9
HibatAllah5 <hibakhaldoune5@gmail.com>
2023-12-13 23:00:12 +0100

    5

--- python-inheritance/5-base_geometry.py
+++ python-inheritance/5-base_geometry.py
@@ -1,18 +1,33 @@
-"""Simple empty BaseGeometry class"""
+"""
+    BaseGeometry module
+"""
+
+
 class BaseGeometry():
-    """Raises Exception
+    """
+        BaseGeometry Class
     """
     def area(self):
-        raise Exception('area() is not implemented')
-    
+        """
+            public instance
+            Raise:
+                Exception: Area not implemented
 
-    """Verify if value is a valid integer value.
-    """    
+        """
+        raise Exception("area() is not implemented")
+
     def integer_validator(self, name, value):
+        """
+            Public instance  that validates value
+            Args:
+                name (string): name
+                value(int): Value
+            Raises:
+                TypeError: When Value is not int
+                ValueError: When Value less or equal to 0
+        """
+        if type(value) is not int:
+            raise TypeError("{} must be an integer".format(name))
+        if value <= 0:
+            raise ValueError("{} must be greater than 0".format(name))
         
-        if name is not None and len(name) > 0:
-            if type(value) is not int:
-                raise TypeError('{:s} must be an integer'.format(name))
-            if value <= 0:
-                raise ValueError('{:s} must be greater than 0'.format(name))
-            

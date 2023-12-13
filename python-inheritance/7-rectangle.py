68a9ee98ee510c45b5179b4b6969400725094db1
HibatAllah5 <hibakhaldoune5@gmail.com>
2023-12-13 21:24:35 +0100

    new

--- python-inheritance/7-rectangle.py
+++ python-inheritance/7-rectangle.py
@@ -1,23 +1,13 @@
 """Simple module with a Rectangle class that inherits BaseGeometry.
 """
-BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
-
-class Rectangle(BaseGeometry):
+class Rectangle():
     """Simple class Rectangle with width, height and input validation.
     """
 
     def __init__(self, width, height):
         """Initialize the rectangle after checking if the values passed are
         valid integers using it's superclass method.
-
-        Arguments:
-            width   {int}   --  a positive non zero integer.
-            height  {int}   --  a positive non zero integer.
-
-        Raises:
-            TypeError:  if any value is not an integer.
-            ValueError: if any value is an integer less or equal to zero.
         """
         super().__init__()
         super().integer_validator('width', width)
@@ -27,19 +17,11 @@ class Rectangle(BaseGeometry):
 
     def area(self):
         """Computes the Area of a Rectangle.
-        Based on the formula AreaRectangle = Height * Width
-
-        Returns:
-            {int} -- The integer value of the Rectangle's Area.
         """
         return self.__height * self.__width
 
     def __str__(self):
         """Generates a human readable string that represents the Rectangle
-
-        Returns:
-            {str} -- The string representation of the Rectangel using the #
-                        character, it represents a 2d rectangle.
         """
         return '[Rectangle] {:d}/{:d}'.format(self.__width, self.__height)
     

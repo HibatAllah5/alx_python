ccb96db18308bc1ff308dec77b55b668235e4691
HibatAllah5 <hibakhaldoune5@gmail.com>
2023-12-13 21:16:21 +0100

    new

--- python-inheritance/5-base_geometry.py
+++ python-inheritance/5-base_geometry.py
@@ -4,24 +4,16 @@ method and a validator function.
 
 """Simple empty BaseGeometry class"""
 class BaseGeometry():
+
     """Prototype of the area function that actually isn't implemented.
-    Raises:
-        Exception:  a simple exception that tells that the area function is
-            not implmenet yet.
     """
 
     def area(self):
         raise Exception('area() is not implemented')
     
+
     """Verify if value is a valid integer value.
-    Arguments:
-        name {str}      --  a string related with the value.
-        value {object}  --  the value that will be checked.
-    Raises:
-            TypeError:  if value is not an integer.
-            ValueError: if value is an integer less or equal to zero.
-        """    
- 
+    """    
     def integer_validator(self, name, value):
         
         if name is not None and len(name) > 0:

ceea33c47090c9c056dd1fc654a2105d4ad9b632
HibatAllah5 <hibakhaldoune5@gmail.com>
2023-12-13 22:29:34 +0100

    new

--- python-inheritance/4-base_geometry.py
+++ python-inheritance/4-base_geometry.py
@@ -1,10 +1,4 @@
-
-"""Simple empty BaseGeometry class"""
 class BaseGeometry():
-
-
-    """Raises an Exception
-    """
     def area(self):
         raise Exception("area() is not implemented")
     

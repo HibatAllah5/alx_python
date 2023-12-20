c15b3e434f80804645c0d75e0c9b68fd3fe2756d
HibatAllah5 <hibakhaldoune5@gmail.com>
2023-12-20 20:03:35 +0100

    header

--- python-network_1/1-hbtn_header.py
+++ python-network_1/1-hbtn_header.py
@@ -1,9 +1,11 @@
 """Takes in URL and email, sends POST request using given parameters, displays
 body of response decoded in utf-8"""
-if __name__ == "__main__":
-    from requests import requests
-    from sys import argv
 
-    with requests.requests(argv[1]) as f:
-        print(f.info().get("X_Request_Id"))
-        
+from requests import requests
+from sys import argv
+
+if __name__ == "__main__":
+    req = requests(argv[1])
+    with requests(req) as f :
+        header = f.getheader('X_Request_Id')
+        print(header)

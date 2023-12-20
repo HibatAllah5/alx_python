6f46ed61d362b9dfff4c9b9973f59025bda7c1db
HibatAllah5 <hibakhaldoune5@gmail.com>
2023-12-20 22:27:38 +0100

    status

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -1,9 +1,10 @@
 """Fetches holberton/ statue page"""
 import requests
 
-r = requests.get("https://alu-intranet.hbtn.io/status")
-with (r) as response:
-    data = response.read()
-    print('Body response:')
-    print('\t- type: {}'.format(type(data)))
-    print('\t- content: {}'.format(data))
+if __name__ == "__main__":
+    r = requests.get("https://alu-intranet.hbtn.io/status")
+    with (r) as response:
+        data = response.read()
+        print('Body response:')
+        print('\t- type: {}'.format(type(data)))
+        print('\t- content: {}'.format(data))

a696dd11d94bf14dca87324321810cb6ab56bd31
HibatAllah5 <hibakhaldoune5@gmail.com>
2023-12-20 21:57:22 +0100

    header

--- python-network_1/1-hbtn_header.py
+++ python-network_1/1-hbtn_header.py
@@ -3,7 +3,7 @@
 if __name__ == "__main__":
     import requests
     from sys import argv
+    url = "https://alu-intranet.hbtn.io/status"
 
     with requests(argv[1]) as f:
         print(f.info().get("X-Request-Id"))
-        

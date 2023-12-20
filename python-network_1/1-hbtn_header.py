9631763a60436e968f12d15173f1ee231617fdbf
HibatAllah5 <hibakhaldoune5@gmail.com>
2023-12-20 23:00:27 +0100

    header

--- python-network_1/1-hbtn_header.py
+++ python-network_1/1-hbtn_header.py
@@ -1,9 +1,12 @@
 """Takes in URL, sends request, displays value of X-Request-Id found in header
 """
-if __name__ == "__main__":
-    import requests
-    from sys import argv
-    url = "https://alu-intranet.hbtn.io/status"
+import requests
+from sys import argv
 
-    with requests(argv[1]) as f:
-        print(f.info().get("X-Request-Id"))
+url = "https://alu-intranet.hbtn.io/status"
+
+request_headers = {"X-Request-Id"}
+
+response = requests.get(request_headers)
+
+print(response.headers)

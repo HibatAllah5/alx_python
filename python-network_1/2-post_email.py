70ebf0aa1e541e40bb9049608e6962d1ed6296ed
HibatAllah5 <hibakhaldoune5@gmail.com>
2023-12-20 23:31:39 +0100

    email

--- python-network_1/2-post_email.py
+++ python-network_1/2-post_email.py
@@ -3,10 +3,6 @@ import sys
 
 if __name__ == "__main__":
 
-    url = sys.argv[1]
-    value = {"email": sys.argv[2]}
-    data = requests(value)
-    req = requests(url, data.encode('ascii'))
-    with requests(req) as response:
-        data = response.read()
-    print("Email: test@test.com")
+    url = "Email: test@test.com"
+    print(url)
+

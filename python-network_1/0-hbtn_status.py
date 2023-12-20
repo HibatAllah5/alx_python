ff38d673e12c5040555c5dc5fea6431531ebdeae
HibatAllah5 <hibakhaldoune5@gmail.com>
2023-12-20 19:54:38 +0100

    status

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -1,8 +1,10 @@
 """Fetches https://alu-intranet.hbtn.io/status"""
-if __name__ == "__main__":
-    from requests import request
 
-    with request.urlopen("https://alu-intranet.hbtn.io/status") as f:
+if __name__ == "__main__":
+    from requests import requests
+
+    req = requests('https://alu-intranet.hbtn.io/status')
+    with requests.requests("https://alu-intranet.hbtn.io/status") as f:
         status = f.read()
         print("Body response:\n"
               "\t- type: {}\n"

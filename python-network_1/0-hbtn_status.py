74f536b0b9df8effdc296f3ce7e1ad7a22e36f97
HibatAllah5 <hibakhaldoune5@gmail.com>
2023-12-20 18:57:12 +0100

    hbtn

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -1,7 +1,7 @@
 """Script that fetches https://intranet.hbtn.io/status and
 prints information about the body of the response retrieved.
 """
-from urllib.request import (urlopen, Request)
+from requests import (urlopen, Request)
 
 
 if __name__ == "__main__":
@@ -13,5 +13,5 @@ if __name__ == "__main__":
         print('\t- type: {}'.format(type(data)))
         print('\t- content: {}'.format(data))
         print('\t- utf8 content: {}'.format(data.decode('utf-8')))
-        
+
         

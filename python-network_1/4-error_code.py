97303576464eff29289605fee783342c762718ef
HibatAllah5 <hibakhaldoune5@gmail.com>
2023-12-21 07:47:21 +0100

    error code

---
+++ python-network_1/4-error_code.py
@@ -0,0 +1,12 @@
+import sys
+import requests
+
+
+if __name__ == "__main__":
+    url = (" ")
+
+    r = requests.get(url)
+    if r.status_code >= 400:
+        print("Error code: {}".format(r.status_code))
+    else:
+        print("index")

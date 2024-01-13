c36c413da1148e7373d54d947365316f4c76d99e
HibatAllah5 <hibakhaldoune5@gmail.com>
2024-01-13 21:33:00 +0100

    states

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -1,15 +1,17 @@
-
 #host: localhost; port: 3306; username: root; password:MYSQL2024@@-Hiba; database:{}.
 
-# Lists all states from the database hbtn_0e_0_usa.
-# Usage: ./0-select_states.py <mysql username> \
-#                             <mysql password> \
-#                             <database name>
-import sys
 import MySQLdb
 
-if __name__ == "__main__":
-    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
-    c = db.cursor()
-    c.execute("SELECT * FROM `states`")
-    [print(state) for state in c.fetchall()]
+host = "localhost"
+port = 3306
+username = "root"
+password = "MYSQL2024@@-Hiba"
+database = "hbtn_0e_0_usa"
+
+connection = MySQLdb.connect(host=host, port=port, user=username, passwd=password, database=database)
+cursor = connection.cursor()
+
+cursor.execute("SELECT * FROM states")
+row = cursor.fetchone()
+
+print(row)

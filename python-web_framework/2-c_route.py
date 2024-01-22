c0cf49862489667b336467b82589387373f956f5
HibatAllah5 <hibakhaldoune5@gmail.com>
2024-01-22 18:58:14 +0100

    new

---
+++ python-web_framework/2-c_route.py
@@ -0,0 +1,28 @@
+"""
+ script that starts a Flask web application
+"""
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route ('/' , strict_slashes=False)
+
+def hello():
+    """
+    This function returns a simple string 'Hello HBNB!' when the root
+    URL ('/') is accessed.
+    
+    """
+    return "Hello HBNB!"
+
+@app.route('/hbnb', strict_slashes=False)
+def text1():
+    return "HBNB"
+
+@app.route('/c/<text>', strict_slashes=False)
+def text2(text):
+    underscore = text.replace('_', ' ')
+    return f'C {underscore}'
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port = 5000)

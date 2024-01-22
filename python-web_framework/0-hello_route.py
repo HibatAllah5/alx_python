74bea94bac2f6fb26a6a6ab4df850e5acd4f2e70
HibatAllah5 <hibakhaldoune5@gmail.com>
2024-01-22 18:48:22 +0100

    new

--- python-web_framework/0-hello_route.py
+++ python-web_framework/0-hello_route.py
@@ -1,15 +1,19 @@
 """
-     script that starts a Flask web application
+ script that starts a Flask web application
 """
 from flask import Flask
 
 app = Flask(__name__)
 
-@app.route ("/strict_slashes=False")
+@app.route ('/' , strict_slashes=False)
 
 def hello():
+    """
+    This function returns a simple string 'Hello HBNB!' when the root
+    URL ('/') is accessed.
+    
+    """
     return "Hello HBNB!"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port = 5000)
-    

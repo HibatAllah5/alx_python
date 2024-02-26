7ec6e8ae9f00a058f32b34984578fb64a4d6f644
HibatAllah5 <hibakhaldoune5@gmail.com>
2024-02-26 20:53:45 +0100

    API

--- api/2-export_to_JSON.py
+++ api/2-export_to_JSON.py
@@ -1,50 +1,41 @@
-import requests
 import json
-
-users_url = "https://jsonplaceholder.typicode.com/users?id="
-todos_url = "https://jsonplaceholder.typicode.com/todos"
-
-
-def get_employee_data(employee_id):
-
-    employee_url = f"https://jsonplaceholder.typicode.com/users/{employee_id}"
-    employee_response = requests.get(employee_url)
-    employee_data = employee_response.json()
+import requests
+import sys
+"""import json, requests, sys"""
 
 
-    todo_url = f"https://jsonplaceholder.typicode.com/users/{employee_id}/todos"
-    todo_response = requests.get(todo_url)
-    todo_data = todo_response.json()
+def getData(id):
+    """
+    Get data from json api and export to json file
+    """
+    usersurl = "https://jsonplaceholder.typicode.com/users/{}".format(id)
+    todourl = "{}/todos".format(usersurl)
 
-    filename = f'{id}.json'
+    request1 = requests.get(usersurl)
+    results = request1.json()
+    userid = results['id']
+    username = results['username']
 
-    employee_tasks = []
-    for task in todo_data:
-        task_info = {
-            "username": employee_data,
-            "task": task["title"],
-            "completed": task["completed"]
-        }
-        employee_tasks.append(task_info)
+    request2 = requests.get(todourl)
+    tasks = request2.json()
+
+    alldata = {}
+
+    jsondata = [
+            {"task": task['title'], "completed": task['completed'], "username": username}
+            for task in tasks
+        ]
     
-    return employee_tasks
+    alldata[str(userid)] = jsondata
 
+    with open("{}.json".format(userid), "w") as jsonfile:
+        json.dump(alldata, jsonfile)
 
-def display_todo_progress(employee_data, todo_data):
-    employee_name = employee_data.get('name')
-    total_tasks = len(todo_data)
-    completed_tasks = sum(task['completed'] for task in todo_data)
-    completed_task_titles = [task['title'] for task in todo_data if task['completed']]
-
-    print(f"Employee {employee_name} is done with tasks({completed_tasks}/{total_tasks}):")
-    for title in completed_task_titles:
-        print(f"\t{title}")
 
 if __name__ == "__main__":
-    if len(sys.argv) != 2:
-        print("Usage: python3 script_name.py <employee_id>")
-        sys.exit(1)
-
-    employee_id = int(sys.argv[1])
-    employee_data, todo_data = get_employee_data(employee_id)
-    display_todo_progress(employee_data, todo_data)
+    if len(sys.argv) > 1:
+        id = sys.argv[1]
+    else:
+        id = 1
+    getData(int(id))
+    
